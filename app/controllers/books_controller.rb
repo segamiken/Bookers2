0ffcf450307f2894a4660dd591e004b7476aae1b
Ken <ken.s,vollyball@gmail.com>
2019-10-01 15:41:29 +0900

    [Add comment] to controller and model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,21 +4,30 @@ class BooksController < ApplicationController
 	def index
 		@book = Book.new
 
+		#search_modelの値によって、Bookを検索するかUserを検索するかが変化
+		#search_wayの値によって、部分一致検索、前方一致検索、後方一致検索、完全一致検索が変化
+		#self.searchはbook.rbに記入
+
 		if params[:search] && params[:search_model] == "1"
 			@books = Book.search(params[:search], params[:search_way])
+
 		elsif params[:search] && params[:search_model] == "2"
 			@users = User.search(params[:search], params[:search_way])
 			render template: "users/index"
+
 		else
 			@books = Book.all
 		end
 
+
+		#検索履歴を保存
 		if params[:search] && params[:order] == nil
 			@history = current_user.search_histories.new
 			@history.history = params[:search]
 			@history.save!
 		end
 
+		#検索結果を非同期でソート
 		if params[:order] && params[:order] == "1"
 			@books_sort = @books.order("id")
 			render :sort
--- app/models/book.rb
+++ app/models/book.rb
@@ -10,9 +10,11 @@ class Book < ApplicationRecord
 
 	def favorited_by?(user)
         favorites.where(user_id: user.id).exists?
-    end
+  end
 
-    def self.search(search, search_way)
+
+  #books controllerのindex actionで使用
+   def self.search(search, search_way)
       if search && search_way == "1"
         where(['title LIKE?', "%#{search}%"])
       elsif search && search_way == "2"
